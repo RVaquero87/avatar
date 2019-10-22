ca92b9e42a1d142f57c0f5ab30191ca5697b44b0
Ruben Vaquero <ruben.vaquero@isobar.com>
2019-10-22 12:42:13 +0200

    Movimiento de objetos en foto

--- common/js/custom.js
+++ common/js/custom.js
@@ -183,6 +183,51 @@ $(document).ready(function () {
 
     });
 
+    var box = $('.avatars-box-picture > div');
+
+    var drag = {
+        elem: null,
+        x: 0,
+        y: 0,
+        state: false
+    };
+    var delta = {
+        x: 0,
+        y: 0
+    };
+
+    box.mousedown(function(e) {
+        if (!drag.state) {
+            drag.elem = this;
+            drag.x = e.pageX;
+            drag.y = e.pageY;
+            drag.state = true;
+        }
+        return false;
+    });
+
+
+    $(document).mousemove(function(e) {
+        if (drag.state) {
+            delta.x = e.pageX - drag.x;
+            delta.y = e.pageY - drag.y;
+            var cur_offset = $(drag.elem).offset();
+            $(drag.elem).offset({
+                left: (cur_offset.left + delta.x),
+                top: (cur_offset.top + delta.y)
+            });
+
+            drag.x = e.pageX;
+            drag.y = e.pageY;
+        }
+    });
+
+    $(document).mouseup(function() {
+        if (drag.state) {
+            drag.state = false;
+        }
+    });
+
 
     //WIDTH Buttons
 
